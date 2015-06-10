80a2ebb1e5a71fae512aa1037814f5249eeb244b
Abdullah Alger <abdullahalger@me.com>
2015-06-10 21:37:29 +0300

    got the authorization working to see posts only for one user

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,23 @@
 class PostPolicy < ApplicationPolicy
+
+  class Scope < Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user, @scope = user, scope
+    end
+
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where(:user_id => user.id) # using just id doesn't work. Had to use user_id.
+      end
+    end
+   end
+
   def index?
     true
   end
+
 end
