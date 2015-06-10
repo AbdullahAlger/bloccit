279fb509d32514be69b03e58745df4149dfebcaf
Abdullah Alger <abdullahalger@me.com>
2015-06-10 09:07:25 +0300

    flash doesn't appear on posts/new or posts/

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :flash_attack
+
+  def flash_attack
+    flash.now[:notice] = "Flash attack"
+  end
+
 end
