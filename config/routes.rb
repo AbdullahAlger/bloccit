43ee12b5b7e8f51bcff321a7bde99c4b8ef1a608
Abdullah Alger <abdullahalger@me.com>
2015-06-15 08:59:28 +0200

    changed the route of summary

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :topics do
     resources :posts, except: [:index] do
-      resource :summary, except: [:index]
+      resource :summary, only: [:show, :new, :create]
     end
   end
 
