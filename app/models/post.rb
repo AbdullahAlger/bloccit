e9f88e427ce8075093e9be2bce3351935083c91b
Abdullah Alger <abdullahalger@me.com>
2015-06-18 13:40:25 +0200

    got ranking working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,18 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_one :summary, dependent: :destroy
+  has_many :votes, dependent: :destroy
   belongs_to :user
   belongs_to :topic
 
   mount_uploader :image, ImageUploader
 
-  scope :ordered_by_title, -> {order('title DESC')}
-  scope :ordered_by_reverse_created_at, -> {order('created_at ASC')}
-  #default_scope {order('created_at DESC')}
+  default_scope {order('rank DESC')}
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+  # validates :topic, presence: true
+  # validates :user, presence: true
 
   def markdown_title
     render_as_markdown title
@@ -21,6 +20,26 @@ class Post < ActiveRecord::Base
 
   def markdown_body
     render_as_markdown body
+  end
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+    new_rank = points + age_in_days
+
+    update_attribute(:rank, new_rank)
   end
 
   private
@@ -31,4 +50,5 @@ class Post < ActiveRecord::Base
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
 end
