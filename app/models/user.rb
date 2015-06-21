40803e4cd723b42c4eb591332fb83d27f24f7cb6
Abdullah Alger <abdullahalger@me.com>
2015-06-21 22:32:49 +0200

    added popular posts and refactored for performance

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,17 @@ class User < ActiveRecord::Base
     votes.where(post_id: post.id).first
   end
 
+  def self.top_rated
+    self.select("users.*")
+        .select("COUNT(DISTINCT comments.id) AS comments_count") # count the comments made by user
+        .select("COUNT(DISTINCT posts.id) AS posts_count") # count the posts made by user
+        .select("COUNT(DISTINCT comments.id) + COUNT(DISTINCT posts.id) AS rank") # add the comment count to the post count and lebel the sum as rank
+        .joins(:posts) # ties the posts table to the users table, via the user_id
+        .joins(:comments) # ties the comments table to the users table, via the user_id
+        .group("users.id") # instructs the database to group the results so that each user will be returned in a distinct row
+        .order("rank DESC") # instructs the database to group the results in descending order, by the rank that we created in this query. (rank = comment count + post count)
+
+
+  end
+
 end
